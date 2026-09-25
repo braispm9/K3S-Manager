1f61719e1bcfb56ee244063e39bc21f1f7ef1c49
braispm9 <braispm9@gmail.com>
2026-09-25 15:45:03 -0400

    Update version message to 1.4

--- k3smanager.sh
+++ k3smanager.sh
@@ -491,7 +491,7 @@ while true; do
             ;;
             
         version)
-            echo "Esta es la versión 1.3.2"
+            echo "Esta es la versión 1.4"
             comprobar_actualizacion
             ;;
         
