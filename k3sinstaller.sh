5411124d9b66113edca8c358330f6574e46c48f1
braispm9 <braispm9@gmail.com>
2026-09-25 13:18:45 -0400

    Add kubectl installation check to k3sinstaller.sh
    Check for kubectl installation and install if missing.

--- k3sinstaller.sh
+++ k3sinstaller.sh
@@ -32,8 +32,15 @@ echo "Instalando para el usuario: $REAL_USER ($REAL_HOME)"
 echo -e "\n1. Verificando dependencias del sistema..."
 
 DEPENDENCIAS=("curl" "fzf")
-snap install kubectl
-
+if command -v kubectl &>/dev/null; then
+    echo "[OK] Kubernetes"
+else
+    apt-get install -y apt-transport-https ca-certificates curl gnupg
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | gpg --dearmor -o /etc/apt/keyring/kubernetes-apt-keyring.gpg
+    echo 'deb [signd-by=/etc/apt/keyring/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    apt-get install -y kubectl
+fi
 for dep in "${DEPENDENCIAS[@]}"; do
     if ! command -v "$dep" &> /dev/null; then
         echo "   [!] '$dep' no está instalado. Intentando instalar..."
